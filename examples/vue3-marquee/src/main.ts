89f6276c45910c4347ca7dbadeba1e868cd637c9
Selemon Dev <selemon@nathandigital.com>
2024-02-22 14:32:21 +0300

    docs: update

--- examples/vue3-marquee/src/main.ts
+++ examples/vue3-marquee/src/main.ts
@@ -2,8 +2,10 @@ import './assets/css/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { Marquee } from "@selemondev/vue3-marquee";
+import { VCodeBlock } from '@wdns/vue-code-block';
 import "@selemondev/vue3-marquee/dist/style.css"
 
 const app = createApp(App);
 app.component('Marquee', Marquee);
+app.component('VCodeBlock', VCodeBlock);
 app.mount('#app')
