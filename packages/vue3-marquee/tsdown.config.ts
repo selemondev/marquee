55d4c46bd3ce8941c5cd034670bd53491a01241e
selemondev <selemondev19@gmail.com>
2025-10-23 15:51:06 -0600

    fix style override

--- packages/vue3-marquee/tsdown.config.ts
+++ packages/vue3-marquee/tsdown.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "tsdown";
 import Vue from "unplugin-vue/rolldown";
 export default defineConfig({
-	entry: ["./src/index.ts"],
-	format: ["esm"],
-	clean: true,
-	minify: true,
-	dts: {
-		vue: true,
-	},
-	target: "chrome100",
-	platform: "browser",
-	plugins: [Vue({ isProduction: true })],
+  entry: ["./src/index.ts"],
+  format: ["esm"],
+  clean: true,
+  minify: true,
+  dts: {
+    vue: true,
+  },
+  target: "chrome100",
+  platform: "browser",
+  plugins: [Vue({ isProduction: true })],
 });
