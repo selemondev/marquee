39f9b5b6aed3b020c751c259e1a33cfa3a5cc1e8
selemondev <selemondev19@gmail.com>
2026-04-16 17:37:17 -0600

    chore: add vercel as the deployment target

--- www/docs/astro.config.mjs
+++ www/docs/astro.config.mjs
@@ -1,13 +1,17 @@
+import { defineConfig } from "astro/config";
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 
 import vue from "@astrojs/vue";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
-export default /** @type {import("astro").AstroUserConfig} */ ({
-  vite: {
+export default defineConfig({
+  /** @type {import("astro").AstroUserConfig} */ vite: {
     plugins: tailwindcss(),
   },
 
   integrations: [vue()],
+  adapter: vercel(),
 });
